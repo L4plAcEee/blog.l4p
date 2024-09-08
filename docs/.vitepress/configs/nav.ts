c88ea0f4a69d8385a6d97adfeff6f15221ab9934
L4plAcEee <1906518568@qq.com>
2024-09-08 23:32:34 +0800

    1.1 -轻量化

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,26 +1,10 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-
   { text: '源码阅读', link: '/doc_analysis/react/18', activeMatch: '^/doc_analysis' },
   {
     text: '施工中...',
-    items: [
-
-    ],
-    activeMatch: '^/doc_learning'
+    items: [],
+    activeMatch: '^/doc_learning',
   },
-  {
-    text: '笔记',
-    items: [
-      { text: '日常笔记', link: '/daily-notes/', activeMatch: '^/daily-notes' },
-      {
-        text: '踩坑记录',
-        items: [
-
-        ]
-      }
-    ],
-    activeMatch: '^/daily-notes|doc_pit'
-  }
 ]
--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,9 +1,6 @@
 import type { DefaultTheme } from 'vitepress'
 import fs from 'fs-extra'
 
-const sidebarDailyNotes: DefaultTheme.SidebarItem[] =
-  fs.readJSONSync('./scripts/daily-notes.json', { throws: false }) || []
-
 export const sidebar: DefaultTheme.Config['sidebar'] = {
   '/doc_learning/': [
     {
@@ -12,28 +9,12 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       items: [
         { text: '[0]', link: '/doc_learning/cpp/[0]' },
         { text: '[1]', link: '/doc_learning/cpp/[1]' },
-      ]
+      ],
     },
     {
       text: 'Python从入门到入土',
       collapsed: false,
-      items: [
-        { text: 'Python 基础', link: '/doc_learning/python/[0]' },
-      ]
-    }
+      items: [{ text: 'Python 基础', link: '/doc_learning/python/[0]' }],
+    },
   ],
-  '/doc_pit/': [
-    {
-      text: '踩坑记录',
-      // collapsed: false,
-      items: [
-        { text: 'npm 踩坑记录', link: '/doc_pit/npm' },
-        { text: '第三方库踩坑记录', link: '/doc_pit/library' },
-        { text: 'PC 踩坑记录', link: '/doc_pit/pc' },
-        { text: 'H5 踩坑记录', link: '/doc_pit/h5' },
-        { text: '微信开发踩坑记录', link: '/doc_pit/wechat' }
-      ]
-    }
-  ],
-  '/daily-notes': sidebarDailyNotes
 }
