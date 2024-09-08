9efd9933b19b5dfb5b9b603407cabb4539ed0d3f
L4plAcEee <1906518568@qq.com>
2024-09-08 22:16:23 +0800

    Revert "Update daily-notes.js"
    This reverts commit b9fbb94ceb71c2ca2da392298975c6d32ab13826.

--- scripts/daily-notes.js
+++ scripts/daily-notes.js
@@ -1,8 +1,8 @@
 import fs from 'fs-extra'
 import { Octokit } from '@octokit/rest'
 
-const owner = ''
-const repo = ''
+const owner = 'L4plAcEee'
+const repo = 'dailyNote'
 
 const octokit = new Octokit()
 
