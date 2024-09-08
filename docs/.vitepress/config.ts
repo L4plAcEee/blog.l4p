a75a7ae6b8e9b0e90f8895c6e1d9ff4b8191c79c
L4plAcEee <1906518568@qq.com>
2024-09-08 21:44:09 +0800

    Revert "1.1"
    This reverts commit 916faabdd715a9a3c85cbe09768b5d727992dedc.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,12 +9,15 @@ const APP_BASE_PATH = basename(process.env.APP_BASE_PATH || '')
 export default defineConfig({
   outDir: '../dist',
   base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+
   lang: 'zh-CN',
   title: 'L4plAce',
   description: '起舞于电子之海',
   head,
+
   lastUpdated: true,
   cleanUrls: true,
+
   /* markdown 配置 */
   markdown: {
     lineNumbers: true,
@@ -22,22 +25,29 @@ export default defineConfig({
       lazyLoading: true,
     },
   },
+
   /* 主题配置 */
   themeConfig: {
     i18nRouting: false,
+
     logo: '/logo.png',
+
     nav,
     sidebar,
+
     /* 右侧大纲配置 */
     outline: {
       level: 'deep',
       label: '目录',
     },
+
     socialLinks: [{ icon: 'github', link: 'https://github.com/L4plAcEee' }],
+
     footer: {
-      message: '如有转载请标注本站原文地址',
+      message: '如有转载或 CV 的请标注本站原文地址',
       copyright: 'Copyright © 2024-present L4plAce',
     },
+
     lastUpdated: {
       text: '最后更新于',
       formatOptions: {
@@ -45,20 +55,39 @@ export default defineConfig({
         timeStyle: 'medium',
       },
     },
+
+    /* Algolia DocSearch 配置 */
     algolia,
+
     docFooter: {
       prev: '上一篇',
       next: '下一篇',
     },
+
     returnToTopLabel: '回到顶部',
     sidebarMenuLabel: '菜单',
     darkModeSwitchLabel: '主题',
     lightModeSwitchTitle: '切换到浅色模式',
     darkModeSwitchTitle: '切换到深色模式',
+
+    /*** 自定义配置 ***/
+    visitor: {
+      badgeId: 'L4plAcEee.notes',
+    },
+
+    comment: {
+      repo: 'L4plAcEee/blog.l4p',
+      repoId: 'R_kgDOMt2tMg',
+      category: 'Announcements',
+      categoryId: 'U_kgDOChSB9g',
+    },
   },
+
+  /* 生成站点地图 */
   sitemap: {
     hostname: 'https://www.l4place.icu/',
   },
+
   vite: {
     plugins: [MarkdownPreview()],
   },
