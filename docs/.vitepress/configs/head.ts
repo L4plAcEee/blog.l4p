22ec2eba7464ae517bb5417aa88d1c4262fb8501
L4palce <1906518568@qq.com>
2024-11-29 22:03:23 +0800

    增加爬虫验证

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -9,6 +9,10 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
   ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
+  
+  // 添加 Algolia 站点验证的 meta 标签
+  ['meta', { name: 'algolia-site-verification', content: 'FCB15DB2015E3224' }],
+
   ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
   ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
